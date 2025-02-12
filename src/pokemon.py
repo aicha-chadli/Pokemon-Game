a021c2c4c0c9710fea53c5a9cff649f3460b17f9
aicha chadli <aicha.chadli@laplateforme.io>
2025-02-12 15:10:55 +0100

    <doc>: Implemented Pokémon class with attributes and API integration using PokeAPI.

--- src/pokemon.py
+++ src/pokemon.py
@@ -1,4 +1,6 @@
-import pygame
+import requests
+import json
+import os
 
 class Pokemon:
     def __init__(self, name, life_points, level, attack_power, defense, type, x, y):
@@ -10,11 +12,11 @@ class Pokemon:
         self.defense = defense
         self.type = type    # water, fire, grass, electric & normal
         # GUI attributes
-        #self.image = pygame.image.load("player.png")
-        #self.image
-        #self.rect = self.image.get_rect(x=x, y=y)
-        #self.speed = 5
-        #self.velocity = [0, 0]  # x, y, x horizontal & vertical control.
+        self.image = pygame.image.load("player.png")
+        self.image
+        self.rect = self.image.get_rect(x=x, y=y)
+        self.speed = 5
+        self.velocity = [0, 0]  # x, y, x horizontal & vertical control.
                                 # 0 is no movement. 
                                 # Accelere if positive, decelere if negative.
 
